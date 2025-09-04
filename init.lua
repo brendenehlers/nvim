9146b3780b96c7f957f019091945529c81a5c917
Brenden Ehlers <brenden@theehlers.org>
2025-09-04 08:33:15 -0600

    add oil git and basic keybinds

--- init.lua
+++ init.lua
@@ -2,5 +2,20 @@ require('config.lazy')
 require('lualine').setup()
 require('oil').setup()
 
+-- map leader to space
+vim.g.mapLeader = ' '
+
 vim.o.relativenumber = true
 vim.o.number = true
+
+
+-- ###############################
+-- ####    KEYMAP SETTINGS    ####
+-- ###############################
+vim.keymap.set('n', '<Leader>t', '<CMD>Oil --float<CR>', { desc = 'open oil in a floating window' })
+
+-- maps for yanking to/pasting from the system register
+vim.keymap.set('v', '<Leader>y', '"+y', { desc = 'yank into system register' })
+vim.keymap.set('v', '<Leader>x', '"+x', { desc = 'cut into system register' })
+vim.keymap.set({'n', 'v'}, '<Leader>p', '"+p', { desc = 'paste from system register' })
+
--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -1,6 +1,12 @@
 return {
-	"stevearc/oil.nvim",
-	lazy = false,
-	dependencies = { { "nvim-tree/nvim-web-devicons", opts = {} } },
-	opts = {},
+	{
+		"stevearc/oil.nvim",
+		lazy = false,
+		dependencies = { { "nvim-tree/nvim-web-devicons", opts = {} } },
+		opts = {},
+	},
+	{
+		"benomahony/oil-git.nvim",
+		dependencies = { "stevearc/oil.nvim" },
+	}
 }
