b7924ace152c259e9917c753de0cb803fa2e1b17
Brenden Ehlers <brenden@theehlers.org>
2025-10-20 19:28:40 -0600

    auto-close configuration

--- init.lua
+++ init.lua
@@ -29,6 +29,15 @@ vim.keymap.set({'n', 'v'}, '<Leader>p', '"+p', { desc = 'paste from system regis
 -- fold keymap
 vim.keymap.set('n', 'zz', 'za', { desc = 'toggle fold' })
 
+-- auto-close keymaps
+vim.keymap.set('i', '(<Tab>', '()<Left>')
+vim.keymap.set('i', '[<Tab>', '[]<Left>')
+vim.keymap.set('i', '{<Tab>', '{}<Left>')
+vim.keymap.set('i', '\'<Tab>', '\'\'<Left>')
+vim.keymap.set('i', '"<Tab>', '""<Left>')
+vim.keymap.set('i', '{<CR>', '{<CR>}<ESC>O')
+vim.keymap.set('i', '{;<CR>', '{<CR>};<ESC>O')
+
 -- ########################################
 -- ####    TOGGLETERM/LAZYGIT SETUP    ####
 -- ########################################
