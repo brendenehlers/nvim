69e79585482980c467e3e8a730cb3ee2638bf322
brenden <brenden@theehlers.org>
2025-10-29 16:04:41 -0600

    disable tabs to spaces

--- init.lua
+++ init.lua
@@ -55,7 +55,7 @@ vim.o.foldexpr = 'v:lua.vim.lsp.foldexpr()'
 
 -- tabbing settings
 vim.o.tabstop = 4 -- A TAB character looks like 4 spaces
-vim.o.expandtab = true -- Pressing the TAB key will insert spaces instead of a TAB character
+vim.o.expandtab = false
 vim.o.softtabstop = 4 -- Number of spaces inserted instead of a TAB character
 vim.o.shiftwidth = 4 -- Number of spaces inserted when indenting
 
